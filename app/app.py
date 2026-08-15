ec1ef491d229427ea685ec86e0e1ccbab79e7eeb
Phasolotl <enderaxgd@gmail.com>
2026-08-15 16:14:50 +0700

    Iniialized base html setup and Flask

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import *
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/sheet')
+def sheet():
+    return render_template('sheet_rosa.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
